f53289b64f825a9e48a9506cb344c6fe39883fb2
Junhyuk Park <junhyeg08@gmail.com>
2023-01-24 07:56:57 +0900

    Feat: 프로그래머스 입문 23일차 2번 문제

--- 00/23_2.go
+++ 00/23_2.go
@@ -3,10 +3,30 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func solution(score [][]int) []int {
-	return []int{}
+	result := make([]int, len(score))
+	temp := []float64{}
+
+	for _, v := range score {
+		n := float64(v[0]+v[1]) / 2
+		temp = append(temp, n)
+	}
+
+	sort.Float64s(temp)
+
+	for i, v := range score {
+		n := float64(v[0]+v[1]) / 2
+		for j, w := range temp {
+			if n == w {
+				result[i] = len(temp) - j
+			}
+		}
+	}
+
+	return result
 }
 
 func main() {
@@ -15,4 +35,7 @@ func main() {
 
 	result2 := solution([][]int{{80, 70}, {70, 80}, {30, 50}, {90, 100}, {100, 90}, {100, 100}, {10, 30}})
 	fmt.Println(result2)
+
+	result3 := solution([][]int{{1, 3}, {3, 1}, {2, 3}, {3, 2}, {1, 0}})
+	fmt.Println(result3)
 }
