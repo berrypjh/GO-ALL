5e6ec2f47890976e899126af092b25ede8fa6725
Junhyuk Park <junhyeg08@gmail.com>
2023-01-29 08:25:10 +0900

    Feat: 프로그래머스 입문 24일차 3번 문제

--- 00/24_3.go
+++ 00/24_3.go
@@ -3,10 +3,29 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func solution(before string, after string) int {
-    return 0
+	str1 := []string{}
+	for _, v := range before {
+		str1 = append(str1, string(v))
+	}
+	sort.Strings(str1)
+
+	str2 := []string{}
+	for _, v := range after {
+		str2 = append(str2, string(v))
+	}
+	sort.Strings(str2)
+
+	for i, v := range str1 {
+		if str2[i] != v {
+			return 0
+		}
+	}
+
+	return 1
 }
 
 func main() {
