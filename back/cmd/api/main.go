d2c75a51bf6da4d587728d23459464d7dc297f96
Junhyuk Park <junhyeg08@gmail.com>
2023-06-07 02:24:19 +0900

    feat: go back-end setting & connecting to postgres

---
+++ back/cmd/api/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
+
+type application struct {
+	DSN string
+	Domain string
+}
+
+func main() {
+	// set application config
+	var app application
+
+	// read from command line
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.Parse()
+
+	// connect to the database
+
+	app.Domain = "example.com"
+
+	log.Println("Starting application on port", port)
+
+	// start a web server
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
